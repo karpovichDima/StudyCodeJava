59a02992778a92aaaaf9b72b4b515af8a8eb1a42
Dima Karpovich <karpovich.code@gmail.com>
2018-06-16 22:14:29 +0300

    Sorting List, MyType, Comparator

--- src/compare/sort.java
+++ src/compare/sort.java
@@ -1,19 +1,15 @@
 package compare;
 
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
-class Person implements Comparable<Person>{
+class Person{
 
-    private int age;
+    int age;
 
     Person(int age){
         this.age = age;
-    }
-
-    @Override
-    public int compareTo(Person p) {
-        return this.age - p.age;
     }
 
     public String toString(){
@@ -21,9 +17,19 @@ class Person implements Comparable<Person>{
     }
 }
 
+class ComparePerson implements Comparator<Person>{
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        return o1.age - o2.age;
+    }
+}
+
 public class sort {
     public static void main(String[] args) {
-        Set<Object> set = new TreeSet<>();
+
+        ComparePerson comparePerson = new ComparePerson();
+        Set<Person> set = new TreeSet<>(comparePerson);
 
         set.add(new Person(4));
         set.add(new Person(6));
